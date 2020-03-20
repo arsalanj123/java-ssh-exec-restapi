0ba1c1a4e46151148a924445b901cbc3ea6ed9ee
Arsalan Jawaid <arsalan.jawaidstu@gmail.com>
2020-03-20 19:14:26 +0400

    Added json response manually and a bit of code cleaning

--- src/main/java/com/example/restservice/SshExecController.java
+++ src/main/java/com/example/restservice/SshExecController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-
 public class SshExecController {
 
+    private static final String template = "%s";
+
     @GetMapping("/api/sshexec")
-    public String getSshDetail(@RequestParam(value="hostname", defaultValue="some host") String hostName, @RequestParam(value="username", defaultValue="some user") String userName, @RequestParam(value="password", defaultValue="some password") String password, @RequestParam(value="sshport", defaultValue="22") String sshPort, @RequestParam(value="command", defaultValue="sh run") String command, @RequestParam(value="stringtofind", defaultValue="find someething") String stringToFind, @RequestParam(value="ifstringfound", defaultValue="found") String ifStringFound, @RequestParam(value="ifstringnotfound", defaultValue="not found") String ifStringNotFound)
-    {
-        SshExec objSshExec = new SshExec("sbx-nxos-mgmt.cisco.com", "admin", "Admin_1234!", "8181", "show running-config eigrp", "authentication mode md5", "EIGRP Authenctication is Secure - Using MD5 Authentication", "EIGRP Authentication is Insecure - Recommended to use MD5 Authentication");
+    public String getSshDetail(@RequestParam(value="hostname", defaultValue="some host") String hostName, @RequestParam(value="username", defaultValue="some user") String userName, @RequestParam(value="password", defaultValue="some password") String password, @RequestParam(value="sshport", defaultValue="22") String sshPort, @RequestParam(value="command", defaultValue="sh run") String command, @RequestParam(value="stringtofind", defaultValue="find someething") String stringToFind, @RequestParam(value="ifstringfound", defaultValue="found") String ifStringFound, @RequestParam(value="ifstringnotfound", defaultValue="not found") String ifStringNotFound) {
+        SshExec objSshExec = new SshExec("", "", "", "", "", "", "", "");
 
         String apiResponse;
 
@@ -22,14 +22,9 @@ public class SshExecController {
 
         String[] response = {hostName, userName, password, sshPort, command, stringToFind, ifStringFound, ifStringNotFound, apiResponse};
 
-        for (int i = 0; i < response.length; i++){
-        System.out.println(response[i]);
-    }
 
-        return apiResponse;
-
-//        return new SshExec ((objSshExec.executeOnDevice(hostName, userName, password, sshPort, command, stringToFind, ifStringFound, ifStringNotFound)), userName, password, sshPort, command, stringToFind, ifStringFound, ifStringNotFound);
+        return "{\"Result\":" + apiResponse + "}";
     }
 }
-
-//http://localhost:8080/api/sshexec?hostname=sbx-nxos-mgmt.cisco.com&username=admin&password=Admin_1234!&sshport=8181&command=show%20running-config%20eigrp&stringtofind=authentication%20mode%20md5&ifstringfound=Eigrp%20Secure&ifstringnotfound=Eigrp%Not%Secure
+//Example of API request:
+//http://localhost:8080/api/sshexec?hostname=sbx-nxos-mgmt.cisco.com&username=admin&password=Admin_1234!&sshport=8181&command=show%20running-config%20eigrp&stringtofind=authentication%20mode%20md5&ifstringfound=Eigrp%20Secure&ifstringnotfound=Eigrp%20Not%20Secure
